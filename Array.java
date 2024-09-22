9acc1e8ad25fe11aa90beacb68161ce1b434b6c7
hamzarezig <rezig3791@gmail.com>
2024-09-22 02:06:12 +0100

    Adding project

---
+++ Array.java
@@ -0,0 +1,234 @@
+import java.util.Iterator;
+/*
+ * an example of dynamic array data structure in java
+ *
+*/
+
+public class Array<T> implements Iterable<T>{ 
+    private int length;
+    private int capacity;
+    private T array[];
+
+    @SuppressWarnings("unchecked")
+    public Array(){
+        length = 0;
+        capacity = 1;
+        array = (T[]) new Object[capacity];
+    }
+    public Iterator<T> iterator(){
+        return new ArrayIterator<T>(this);
+    }
+
+    public int length(){
+        return length;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void add(T element){
+        if(length ==  capacity){
+            capacity *=2;
+            T[] newArr = (T[]) new Object[capacity];
+
+            for(int i=0;i<length;i++) 
+                newArr[i] = array[i];
+
+            array = newArr;
+        }
+        array[length++] = element;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void add(int index,T element){
+        if(length ==  capacity){
+            capacity *=2;
+            T[] newArr = (T[]) new Object[capacity];
+
+            for(int i=0;i<index;i++) 
+                newArr[i] = array[i];
+            for(int i=length-1;i>=index;i--)
+                newArr[i+1] = array[i];
+
+            array = newArr;
+        }
+        else
+            for(int i=length-1;i>=index;i--)
+                array[i+1] = array[i];
+        array[index] = element;
+        length++;
+    }
+
+    public T get(int index){
+        // check bounds to avoid invalid access
+        if(index >= length || index <= -1){
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds!");
+        }
+
+        return array[index];
+    }
+
+    public void set(int index, T element ){
+        // check bounds to avoid invalid access
+        if(index >= length || index <= -1){
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds!");
+        }
+        array[index] = element;
+
+    }
+
+    public void remove(int index){
+        // check bounds to avoid invalid access
+        if(index >= length || index <= -1){
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds!");
+        }
+
+        for(int i=index;i<length-1;i++){
+            array[i]=array[i+1];
+        }
+        length--;
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public void clear(){
+        length = 0;
+        capacity = 1;
+        array = (T[]) new Object[capacity];
+
+    }
+
+    public boolean contains(T element){
+        for(int i=0;i<length;i++){
+            if(array[i].equals(element)) return true;
+        }
+        return false;
+    }
+
+    public int indexOf(T element){
+        for(int i=0;i<length;i++){
+            if(array[i].equals(element)) return i;
+        }
+        return -1;
+    }
+    public boolean isEmpty(){
+        return length==0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void shrink(){
+        if(length == capacity)
+            return;
+        T[] newArr = (T[]) new Object[length];
+
+        for(int i=0;i<length;i++)
+            newArr[i] = array[i];
+        array = newArr;
+    }
+
+    @Override
+    public String toString(){
+        if(length == 0) return "[]";
+        String output="[";
+        
+        for(int i=0;i<length;i++){
+            if(i!=0){
+                output+=", ";
+            }
+            output+=""+array[i];
+
+        }
+        output+="]";
+        return output;
+    }
+
+
+    public static void main(String[] args){
+        // creating array
+        Array<Integer> arr = new Array<>();
+        // adding to the array
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+
+        // replaces (index, value)
+        arr.set(1,3);
+        arr.set(3,7);
+
+        // add with index (index,value)
+        arr.add(2,3);
+        arr.add(3,3);
+
+        // shrinks the capacity of the array to match the length
+        arr.shrink();
+
+        // getting current length
+        int len = arr.length();
+        // looping through the array
+        for(int i=0;i<len;i++){
+            System.out.println(arr.get(i));
+        }
+        // loop with for each 
+        for(Integer element : arr){
+            System.out.println(element);
+        }
+        // show it as string 
+        System.out.println(arr.toString());
+
+        // check if the array contains a certain element and returns a boolean
+        System.out.println(arr.contains(1));
+        System.out.println(arr.contains(2));
+        
+        // return the index of the element or -1 if it doesn't exist 
+        System.out.println(arr.indexOf(1));
+        System.out.println(arr.indexOf(2));
+
+        // testing out of bounds
+        // System.out.println(arr.get(100)); 
+        // arr.remove(100);
+        // arr.set(100,1337);
+        
+        // checks if the array is empty : false
+        System.out.println(arr.isEmpty());
+
+        // removing from the array
+        arr.remove(0);
+        arr.remove(0);
+        arr.remove(0);
+        arr.remove(0);
+
+        // or clear it all with clear()
+        arr.clear();
+
+        // checks if the array is empty : true
+        System.out.println(arr.isEmpty());
+
+    }
+    
+}
+// the iterator class 
+class ArrayIterator<T> implements Iterator<T>{
+    private int index;
+    private int length;
+    private Array<T> array;
+
+    public ArrayIterator(Array<T> array){
+        index = 0;
+        length = array.length();
+        this.array = array;
+
+    }
+    public boolean hasNext(){
+        return index != length;
+
+    }
+    public T next(){
+        T data = array.get(index);
+        index++;
+        return data;
+    }
+    public void remove(){
+        throw new UnsupportedOperationException();
+    }
+
+
+}
---
+++ DoublyLinkedList.java
@@ -0,0 +1,258 @@
+import java.util.NoSuchElementException;
+
+// node class
+class Node<T>{
+    public T value;
+    public Node<T> next;
+    public Node<T> prev;
+
+    public Node(T value, Node<T> next, Node<T> prev){
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+public class DoublyLinkedList<T> {
+    int size ;
+    Node<T> head;
+    Node<T> tail;
+
+    public DoublyLinkedList(){
+        size = 0;
+        head = null;
+        tail = null;
+    }
+    // insertion methods + array insertion
+    public void add(T value){
+        addLast(value);
+    }
+    public void add(T[] arr){
+        addLast(arr);
+    }
+    public void addLast(T value){
+        if(size==0)
+            head = tail = new Node<T>(value,null,null);
+        else{
+            tail.next = new Node<T>(value,null,tail);
+            tail = tail.next;
+        }
+        size++;
+    }
+    public void addLast(T[] arr){
+        for(int i=0;i<arr.length;i++){
+            addLast(arr[i]);
+        }
+    }
+    public void addFirst(T value){
+        if(size==0)
+            head = tail = new Node<T>(value,null,null);
+        else{
+            head.prev = new Node<T>(value,head,null);
+            head = head.prev;
+        }
+        size++;
+        
+    }
+    public void addFirst(T[] arr){
+        for(int i=arr.length-1;i>=0;i--){
+            addFirst(arr[i]);
+        }
+    }
+    public void addAt(int index,T value){
+        if(index>size) throw new IndexOutOfBoundsException("Index " + index + " is out of bounds!");
+        if(index==0){
+            addFirst(value);
+            return;
+        } 
+        if(index==size){
+            addLast(value);
+            return;
+        } 
+        Node<T> node;
+        if(index<=size/2){
+            node=head;
+            for(int i=0;i<index;i++) node = node.next;
+        }else{
+            node=tail;
+            for(int i=size-1;i>index;i--) node = node.prev;
+        }
+        node.prev.next = new Node<T>(value,node,node.prev);
+        node.prev = node.prev.next;
+        size++;
+    }
+
+    // deletion methods
+    private void remove(Node<T> node){
+        if(node.prev == null){
+            removeFirst();
+            return;
+        }
+        if(node.next == null){
+            removeLast();
+            return;
+        }
+
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
+
+        node.value = null;
+        node.next = null;
+        node.prev = null;
+
+        size--;
+    }
+    public void removeFirst(){
+        if(isEmpty()) throw new IllegalArgumentException();
+        head = head.next;
+        size--;
+        if(isEmpty()) tail = null;
+        else head.prev = null;
+    }
+    public void removeLast(){
+        if(isEmpty()) throw new IllegalArgumentException();
+        tail = tail.prev;
+        size--;
+        if(isEmpty()) head = null;
+        else tail.next = null;
+    }
+    public void removeAt(int index){
+        if(index < 0 || index >= size) throw new IllegalArgumentException();
+        if(index==0) {
+            removeFirst();            
+            return;
+        }
+        if(index==size-1){
+            removeLast();
+            return;
+        }
+        Node<T> node;
+        if(index<size/2){
+            node=head;
+            for(int i=0;i<index;i++) node=node.next;
+        }else{
+            node=tail;
+            for(int i=size-1;i>index;i--) node=node.prev;
+        }
+        remove(node);
+    }
+
+    // utility methods
+    public void clear(){
+        head = tail = null;
+        size=0;
+    }
+    public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    // access methods
+    public T getFirst(){
+        if(isEmpty()) throw new NoSuchElementException("List is empty");
+        return head.value;
+    }
+    public T getLast(){
+        if(isEmpty()) throw new NoSuchElementException("List is empty");
+        return tail.value;
+    }
+    public T getAt(int index){
+        if (index < 0 || index >= size) throw new IllegalArgumentException();
+        Node<T> node;
+        if(index<=size/2){
+            node=head;
+            for(int i=0;i<index;i++) node = node.next;
+        }else{
+            node=tail;
+            for(int i=size-1;i>index;i--) node = node.prev;
+        }
+        return node.value;
+    }
+    
+    //update methods 
+    public void setAt(int index,T value){
+        Node <T> node;
+        if(index<=size/2){
+            node=head;
+            for(int i=0;i<index;i++) node = node.next;
+        }else{
+            node=tail;
+            for(int i=size-1;i>index;i--) node = node.prev;
+
+        }
+        node.value=value;
+        
+    }
+
+    @Override
+    public String toString(){
+        if(size == 0) return "[]";
+        Node <T> node=head;
+        StringBuilder str = new StringBuilder();
+        str.append("[");
+        str.append(node.value);
+        node = node.next;
+        for(int i=1;i<size;i++){
+            str.append(", ");
+            str.append(node.value);
+            node = node.next;
+        }
+        str.append("]");
+        return str.toString();
+    } 
+    public static void main(String[] args){ // TODO : make test cases 
+        // creating the list 
+        DoublyLinkedList<Integer> list =  new DoublyLinkedList<>();
+        // adding the to list 
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.addLast(4); // add or addLast are the same
+        // add 0 to the first of the list 
+        list.addFirst(0);
+
+        System.out.println("List:\n"+list);
+        // accessing the list
+        System.out.println("First element: "+list.getFirst()); // get first element 
+        System.out.println("Last element: "+list.getLast()); // get last element
+        System.out.println("Element at index 2: "+list.getAt(2)); // get element at certain index
+
+        System.out.println("List size: "+list.size()); // get list size 
+        System.out.println("List is empty? "+list.isEmpty()); // check whether the list is empty or not (size == 0)
+
+        //inserting in the list
+        list.addAt(2,4); // addAt(index , value)
+        System.out.println("List after adding 4 at index 2:\n"+ list);// print the list 
+
+        // removing from the list
+        list.removeFirst(); // remove first element
+        System.out.println("List after removing first element:\n"+ list);
+        list.removeLast(); // remove last element
+        System.out.println("List after removing last element:\n"+ list);
+
+        // adding to the list but with arrays 
+        Integer[] arr1 = {6, 7, 8};
+        list.addLast(arr1); // adding an array to the end of the list 
+        System.out.println("List after adding {6, 7, 8} to the list:\n"+list);
+
+        Integer[] arr2 = {-2, -1};
+        list.addFirst(arr2); // adding an array to the beginning of the list 
+        System.out.println("List after adding {6, 7, 8} to the list:\n"+list);
+        
+        // updating at certain index 
+        list.setAt(3, 10); // update element at index 3 to 10
+        System.out.println("List after setting element at index 3 to 10:\n"+list);
+
+        // clearing the list 
+        list.clear(); // clear the list from all the elment within O(1)
+        System.out.println("List after clearing:\n"+list);
+        
+        System.out.println("List is empty? "+list.isEmpty());
+
+
+
+    } 
+    
+}
